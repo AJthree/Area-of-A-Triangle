05c00fe53a92eaef274a081368b2aea740db16b2
Alfred James III <97080006+AJthree@users.noreply.github.com>
2022-02-12 15:00:30 -0600

    Add files via upload

---
+++ AREA OF A TRIANGLE PROGRAM.py
@@ -0,0 +1,7 @@
+#Create a program that will calculate the area of a triangle
+print("This program will calculate the area of a triangle")
+print("Please enter the dimensions of your triangle below.")
+base = input("Please enter the value of base: ")
+height = input("Please enter the value of height: ")
+area = 1 * float(base) * int(height)
+print("The area of your triangle is: " + str(area))
